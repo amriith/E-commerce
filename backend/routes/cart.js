ea6b335b8664f51bc529440e95e7602854ce8d36
amriith <amrithjayadeep916@gmail.com>
2024-12-28 21:21:40 +1100

    Day 8 add view-cart route to retrieve user's cart items

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -62,4 +62,29 @@ catch (err) {
 }
 })
 
+router.get("/view-cart", authMiddleWare, async (req, res) =>{
+  try { const userId = req.userId;
+    if(!ObjectId.isValid(userId)){
+        return res.status(403).json({
+            message: "Invalid User Id"
+        })
+    }
+    const user =await User.findById(userId);
+    if(!user){
+        return res.status(403).json({
+            message: "User not found"
+        })
+    }
+    const cartItems = user.cart;
+    res.status(200).json({
+        message: "Success",
+        cartItems
+    })
+   
+}
+    catch(err){
+        res.status(500).json({ message: 'Error fetching cart items', error: err.message });
+    }
+})
+
 module.exports = router;
