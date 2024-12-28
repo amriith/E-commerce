56f54bac8a1a0d7d83842e44e02091a559472398
amriith <amrithjayadeep916@gmail.com>
2024-12-28 14:01:46 +1100

    Day 8 add-to-cart route added

--- backend/db.js
+++ backend/db.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost:27017/mriid')
         console.error("MongoDB connection error:", err);
     });
 
+    
 
 
 const userSchema =  mongoose.Schema({
--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,10 +1,65 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const router = express.Router();
 const {authMiddleWare} = require("../middleware");
 const {Product ,User} = require("../db");   
+const { ObjectId } = mongoose.Types;
 
 router.post("/add-to-cart", authMiddleWare, async (req, res) =>{
+const {productId, quantity , size, color} = req.body;
+const userId = req.userId
 
+try{
+    if (!ObjectId.isValid(userId) || !ObjectId.isValid(productId)) {
+        return res.status(400).json({ message: "Invalid userId or productId" });
+    }
+    const user = await User.findById(userId);
+    const product = await Product.findById(productId);
+    if(!product ||  !user){
+        return res.status(403).json({
+            message: "Please Add a valid Product/ Try logging in Again"
+        })
+    }
+    const variations = product.variations.find(v=> v.size === size && v.color === color);
+
+    if(!variations){
+        return res.status(403).json({
+            message: "Please select a valid size and color"
+        })
+    }
+     
+    const cartItem = user.cart.find(item => item.productId.equals(productId) && item.size === size && item.color === color);
+    
+    if (cartItem && variations.stock < cartItem.quantity + quantity) {
+        return res.status(403).json({
+            message: "Not enough stock available"
+        });
+    }
+    if (!cartItem && variations.stock < quantity) {
+        return res.status(403).json({
+            message: "Not enough stock available"
+        });
+    }
+
+    if(cartItem){
+        cartItem.quantity += quantity
+    }
+    else{
+        user.cart.push({
+            productId,
+            size,
+            color,
+            quantity
+        })
+    }
+    await user.save();
+    res.status(200).json({
+        message: "Product added to cart successfully"
+    })
+}
+catch (err) {
+    res.status(500).json({ message: 'Error adding product to cart', error: err.message });
+}
 })
 
 module.exports = router;
